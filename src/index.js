04462544ba1b3cb7bf03cfc23bbb823b850b3c1b
Prakhar <prakharshrivastava109@gmail.com>
2026-07-22 11:44:33 +0530

    forming utilities

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ dotenv.config({
 });
 
 connectDB()
+.then( () =>{
+    app.on("error" , (err) => {
+        console.log("Error while connecting to the server : ", err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB CONNECTION FAILED!! : ", error);
+})
