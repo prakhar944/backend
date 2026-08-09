07eff06a6c1c52196a37dd4a83cef5eb23663f8b
Prakhar <prakharshrivastava109@gmail.com>
2026-08-09 08:24:15 +0530

    add getWatchHistory controller to fetch user's watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -424,6 +424,60 @@ const getUserChannelProfile = asyncHandler( async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler( async (req, res) => {
+   const user = await User.aggregate([
+      {
+         $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         } 
+      },
+      {
+         $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignfield: "_id",
+            as: "watchHistory",
+            pipeline: [
+               {
+                  $lookup: {
+                     from: "users",
+                     localfield: 'owner',
+                     foreignfield: "_id",
+                     as: "owner",
+                     pipeline: [
+                        {
+                           $project: {
+                              fullName: 1,
+                              username: 1,
+                              avatar: 1
+                           }
+                        }
+                     ]
+                  }
+               },
+               {
+                  $addFields: {
+                     owner: {
+                        $first: "$owner"
+                     }
+                  }
+               }
+            ]
+         }
+      }
+   ])
+
+   return res
+   .status(200)
+   .json(
+      new ApiResponse(
+         200,
+         user[0]?.watchHistory,
+         "Watch History Fetched Successfully"
+      )
+   )
+})
+
 export { 
    registerUser,
    loginUser,
@@ -434,5 +488,6 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
 } 
