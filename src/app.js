18b0b3689483b139275e526d5f3a70efbe8066a0
Prakhar <prakharshrivastava109@gmail.com>
2026-08-31 14:51:09 +0530

    implement healthcheck functionality with routes and controller for server health status

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ import playlistRouter from "./routes/playlist.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import videoRouter from "./routes/video.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // ROUTES DECLARATION
 app.use("/api/v1/users", userRouter)
@@ -40,5 +41,6 @@ app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/videos", videoRouter)
+app.use("api/v1/healthchecks", healthcheckRouter)
 
 export { app }
